08885bf7aa583cf1df2ce9961775d3bf0a3efc3c
zakski <zak.szadowski+git@gmail.com>
2016-10-28 17:35:01 +0100

    Adding Skeleton of the people framework

---
+++ src/main/scala/com/szadowsz/maeve/babby/data/gender/GenderUtil.scala
@@ -0,0 +1,17 @@
+package com.szadowsz.maeve.babby.data.gender
+
+/**
+  * Created on 28/10/2016.
+  */
+object GenderUtil {
+
+
+  def forName(gen: String): Gender = {
+    gen.toLowerCase() match {
+      case x if Gender.BOY.synonyms.contains(x) => Gender.BOY
+      case y if Gender.GIRL.synonyms.contains(y) => Gender.GIRL
+      case _ => Gender.BOTH
+    }
+  }
+
+}
---
+++ src/main/scala/com/szadowsz/maeve/babby/target/census/usa/PopularNamesExecutor.scala
@@ -0,0 +1,8 @@
+package com.szadowsz.maeve.babby.target.census.usa
+
+/**
+  * Created on 28/10/2016.
+  */
+class PopularNamesExecutor {
+
+}
